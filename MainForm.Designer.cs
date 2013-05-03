197be2b346c4b43339fd781fe19e1d298bf5d12f
KONG LINGJIAN <KEKOXU@GMAIL.COM>
2013-05-03 12:22:15 +0800

    测试版上传

---
+++ MainForm.Designer.cs
@@ -0,0 +1,196 @@
+﻿/*
+ * 由SharpDevelop创建。
+ * 用户： Steven Kong
+ * 日期: 2013/1/31
+ * 时间: 18:14
+ */
+namespace Logcq
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.button1 = new System.Windows.Forms.Button();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.URL = new System.Windows.Forms.TextBox();
+			this.Post = new System.Windows.Forms.TextBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.button2 = new System.Windows.Forms.Button();
+			this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+			this.Cookie = new System.Windows.Forms.TextBox();
+			this.button3 = new System.Windows.Forms.Button();
+			this.label4 = new System.Windows.Forms.Label();
+			this.refer = new System.Windows.Forms.TextBox();
+			this.SuspendLayout();
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(167, 180);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(86, 40);
+			this.button1.TabIndex = 0;
+			this.button1.Text = "Post";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.Button1Click);
+			// 
+			// label2
+			// 
+			this.label2.Font = new System.Drawing.Font("宋体", 12F);
+			this.label2.Location = new System.Drawing.Point(34, 17);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(42, 23);
+			this.label2.TabIndex = 2;
+			this.label2.Text = "URL:";
+			this.label2.TextAlign = System.Drawing.ContentAlignment.TopRight;
+		 
+			// 
+			// label3
+			// 
+			this.label3.Font = new System.Drawing.Font("宋体", 12F);
+			this.label3.Location = new System.Drawing.Point(26, 52);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(50, 21);
+			this.label3.TabIndex = 3;
+			this.label3.Text = "Post:";
+			this.label3.TextAlign = System.Drawing.ContentAlignment.TopRight;
+			// 
+			// URL
+			// 
+			this.URL.Location = new System.Drawing.Point(85, 17);
+			this.URL.MaxLength = 9999;
+			this.URL.Name = "URL";
+			this.URL.Size = new System.Drawing.Size(236, 21);
+			this.URL.TabIndex = 5;
+			// 
+			// Post
+			// 
+			this.Post.Location = new System.Drawing.Point(85, 52);
+			this.Post.MaxLength = 9999;
+			this.Post.Multiline = true;
+			this.Post.Name = "Post";
+			this.Post.Size = new System.Drawing.Size(236, 21);
+			this.Post.TabIndex = 6;
+			// 
+			// label1
+			// 
+			this.label1.Font = new System.Drawing.Font("宋体", 12F);
+			this.label1.Location = new System.Drawing.Point(4, 92);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(72, 27);
+			this.label1.TabIndex = 7;
+			this.label1.Text = "Cookie:";
+			this.label1.TextAlign = System.Drawing.ContentAlignment.TopRight;
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(52, 178);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(78, 42);
+			this.button2.TabIndex = 9;
+			this.button2.Text = "GET";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.Button2Click);
+			// 
+			// richTextBox1
+			// 
+			this.richTextBox1.Location = new System.Drawing.Point(327, 17);
+			this.richTextBox1.Name = "richTextBox1";
+			this.richTextBox1.Size = new System.Drawing.Size(207, 203);
+			this.richTextBox1.TabIndex = 10;
+			this.richTextBox1.Text = "";
+			// 
+			// Cookie
+			// 
+			this.Cookie.Location = new System.Drawing.Point(85, 92);
+			this.Cookie.MaxLength = 9999;
+			this.Cookie.Name = "Cookie";
+			this.Cookie.Size = new System.Drawing.Size(236, 21);
+			this.Cookie.TabIndex = 11;
+			// 
+			// button3
+			// 
+			this.button3.Location = new System.Drawing.Point(388, 226);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(75, 23);
+			this.button3.TabIndex = 12;
+			this.button3.Text = "清空";
+			this.button3.UseVisualStyleBackColor = true;
+			this.button3.Click += new System.EventHandler(this.Button3Click);
+			// 
+			// label4
+			// 
+			this.label4.Font = new System.Drawing.Font("宋体", 12F);
+			this.label4.Location = new System.Drawing.Point(20, 128);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(64, 23);
+			this.label4.TabIndex = 13;
+			this.label4.Text = "Refer:";
+			// 
+			// refer
+			// 
+			this.refer.Location = new System.Drawing.Point(85, 128);
+			this.refer.Name = "refer";
+			this.refer.Size = new System.Drawing.Size(236, 21);
+			this.refer.TabIndex = 14;
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(553, 261);
+			this.Controls.Add(this.refer);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.button3);
+			this.Controls.Add(this.Cookie);
+			this.Controls.Add(this.richTextBox1);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.Post);
+			this.Controls.Add(this.URL);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.button1);
+			this.Name = "MainForm";
+			this.Text = "GetIWant";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+		}
+		private System.Windows.Forms.TextBox refer;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.Button button3;
+		private System.Windows.Forms.TextBox Cookie;
+		private System.Windows.Forms.RichTextBox richTextBox1;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.TextBox Post;
+		private System.Windows.Forms.TextBox URL;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Button button1;
+	}
+}
---
+++ MainForm.cs
@@ -0,0 +1,99 @@
+﻿/*
+ * 由SharpDevelop创建。
+ * 用户： Steven Kong
+ * 日期: 2013/1/31
+ * 时间: 18:14
+ * 
+ * 要改变这种模板请点击 工具|选项|代码编写|编辑标准头文件
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Net;
+using System.Text;
+using System.IO;
+namespace Logcq
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		public  MainForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+		
+		}
+		string head = "";
+        string str = System.IO.Directory.GetCurrentDirectory();
+		
+		void Button1Click(object sender, EventArgs e)
+		{
+			string htmlp = http.post(URL.Text,Post.Text,Cookie.Text,refer.Text,head);
+			richTextBox1.AppendText(htmlp);
+		
+			write(htmlp);
+			
+	    }
+		public  string Search_string(string s, string s1, string s2)  //获取搜索到的数目
+        {
+            int n1, n2;
+            n1 = s.IndexOf(s1, 0) + s1.Length;   //开始位置
+            n2 = s.IndexOf(s2, n1);               //结束位置
+            return s.Substring(n1, n2 - n1);   //取搜索的条数，用结束的位置-开始的位置,并返回
+        }
+ 
+		
+		void Button2Click(object sender, EventArgs e)
+		{
+			
+			string htmll = http.Gethtml(URL.Text,Cookie.Text);
+			richTextBox1.AppendText(htmll);
+		
+			write(htmll);
+			
+			
+		}
+		public void write(string word)
+		{
+			
+			                         StreamWriter sr;
+             if (File.Exists(str+"\\load.txt")) //如果文件存在,则创建File.AppendText对象
+             {
+                 sr = File.AppendText(str+"\\load.txt");
+             }
+             else   //如果文件不存在,则创建File.CreateText对象
+             {
+                 sr = File.CreateText(str+"\\load.txt");
+             }
+            // sr.WriteLine(str);
+             sr.Close();
+           //创建一个文件流，用以写入或者创建一个StreamWriter
+            FileStream fs = new FileStream(str+"\\load.txt", FileMode.OpenOrCreate, FileAccess.Write);
+            StreamWriter m_streamWriter = new StreamWriter(fs);
+            m_streamWriter.Flush();
+            // 使用StreamWriter来往文件中写入内容
+            m_streamWriter.BaseStream.Seek(0, SeekOrigin.End);
+            m_streamWriter.Write(word+"\r\n\r\n\r\n");
+
+            //关闭此文件
+            m_streamWriter.Flush();
+            m_streamWriter.Close();
+            
+		}
+
+
+		
+		void Button3Click(object sender, EventArgs e)
+		{
+           richTextBox1.Text="";			
+		}
+	}
+}
